5b24efad46520255b7e583e1aaf16bf9b1bacd02
Christopher Pruitt <pruicr@gmail.com>
2017-01-24 17:27:37 -0800

    add ajax request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import $ from 'jquery'
 
 import './App.css'
 import About from './Components/About'
@@ -10,7 +11,34 @@ import Resume from './Components/Resume'
 import Testimonials from './Components/Testimonials'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      resumeData: {}
+    }
+  }
+
+  getResumeData() {
+    $.ajax({
+      url: 'http://localhost:3000/resumeData.json',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({resumeData: data})
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err)
+        alert(err)
+      }
+    })
+  }
+
+  componentDidMount() {
+    this.getResumeData()
+  }
+
   render() {
+    console.log(this.state.resumeData)
     return (
       <div className="App">
         <Header/>
